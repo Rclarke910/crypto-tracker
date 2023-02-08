11a7937522b9566ff3e06f5675d218c7b968501b
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2023-02-08 06:49:19 -0500

    test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const filteredCoins = coins.filter(coin =>
   return (
     <div>
     <h1 className = 'app__header--title'>Today In Crypto!</h1>
-    <img src="public/Crypto-img.png" className='app__header--img'/>
+    <img src="build/Crypto-img.png" className='app__header--img'/>
     <form>
       <input type='textarea' className = 'app__searchbar--bar' placeholder="Search For A Coin" onChange = {handleChange}/>
       </form>
